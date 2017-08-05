04c1c5600da87724ebeee5767037b8128d46d786
Ananth3990 <ananthchandrasekharan@gmail.com>
2017-08-05 17:51:39 +0530

    Done by function as well as Matrix form
    Done by function as well as matrix form.

---
+++ Assignment_09/Function_Form/Assignment_01/CubeRotation_withNoBase/Pyramid3.cpp
@@ -0,0 +1,375 @@
+#include<Windows.h>
+#include<math.h>
+#include<gl/GL.h>
+#include<gl/GLU.h>
+#include<math.h>
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+#pragma comment(lib,"opengl32.lib")
+#pragma comment (lib,"glu32.lib")
+
+HWND ghwnd = NULL;
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+bool gbActiveWindow = false;
+bool gbEscapeKeyIsPressed = false;
+bool gbFullScreen = false;
+
+#define NUMBER_OF_ROWS 4
+#define NUMBER_OF_COLOUMNS 4
+
+GLfloat anglePyramid = 0.0f;
+LRESULT CALLBACK AcCallBack(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+void update(void)
+{
+	if (gbFullScreen == true)
+	{
+		anglePyramid = anglePyramid + 0.15f;
+	}
+	else
+	{
+		anglePyramid = anglePyramid + 0.1f;
+	}
+
+	if (anglePyramid >= 360.0f)
+	{
+		anglePyramid = 0.0f;
+	}
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	void initialize(void);
+	void uninitialize(void);
+	void display(void);
+	void update(void);
+	void resize(int,int);
+
+	WNDCLASSEX ac;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szClassName[] = TEXT("Rotating Pyramid");
+	bool bDone = false;
+
+	ac.cbSize = sizeof(WNDCLASSEX);
+	ac.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	ac.cbClsExtra = 0;
+	ac.cbWndExtra = 0;
+	ac.hInstance = hInstance;
+	ac.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	ac.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	ac.hCursor = LoadCursor(NULL, IDC_ARROW);
+	ac.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	ac.lpfnWndProc = AcCallBack;
+	ac.lpszClassName = szClassName;
+	ac.lpszMenuName = NULL;
+
+	RegisterClassEx(&ac);
+
+	hwnd = CreateWindowEx(
+		WS_EX_APPWINDOW,
+		szClassName,
+		TEXT("Shree Ganesha"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		0,
+		0,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ghwnd = hwnd;
+
+	initialize();
+
+	ShowWindow(hwnd, SW_SHOWNORMAL);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	while (bDone == false)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				bDone = true;
+			}
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow == true)
+			{
+				update();
+				display();
+				if (gbEscapeKeyIsPressed == true)
+				{
+					bDone = true;
+				}
+			}
+		}
+	}
+	uninitialize();
+	return ((int)msg.wParam);
+}
+
+void initialize(void)
+{
+	void resize(GLint width, GLint height);
+
+	PIXELFORMATDESCRIPTOR pfd;
+	GLint iPixelFormatIndex;
+
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cAlphaBits = 8;
+	pfd.cDepthBits = 32;
+
+	ghdc = GetDC(ghwnd);
+
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == false)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	ghrc = wglCreateContext(ghdc);
+	if (ghrc == NULL)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	resize(WIN_WIDTH, WIN_HEIGHT);
+
+}
+
+void uninitialize(void)
+{
+	if (gbFullScreen == true)
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOZORDER | SWP_NOOWNERZORDER | SWP_NOMOVE | SWP_NOSIZE | SWP_FRAMECHANGED);
+		ShowCursor(TRUE);
+	}
+
+	wglMakeCurrent(NULL, NULL);
+	wglDeleteContext(ghrc);
+	ghrc = NULL;
+	ReleaseDC(ghwnd, ghdc);
+	ghdc = NULL;
+	DestroyWindow(ghwnd);
+	ghwnd = NULL;
+
+}
+
+void resize(GLint width, GLint height)
+{
+	if (height == 0)
+	{
+		height = 1;
+	}
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+}
+
+void ToggleFullScreen(void)
+{
+	MONITORINFO mi;
+
+	if (gbFullScreen == false)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		if (dwStyle&WS_OVERLAPPEDWINDOW)
+		{
+			mi = { sizeof(MONITORINFO) };
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle&~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+		}
+		ShowCursor(FALSE);
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOZORDER | SWP_NOOWNERZORDER | SWP_NOMOVE | SWP_NOSIZE | SWP_FRAMECHANGED);
+		ShowCursor(TRUE);
+	}
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, -6.0f);
+	glRotatef(anglePyramid, 0.0f, 1.0f, 0.0f);
+
+	glBegin(GL_TRIANGLES);
+
+	//Front face
+	glColor3f(1.0f, 0.0f, 0.0f); 
+	glVertex3f(0.0f, 1.0f, 0.0f); //apex
+
+	glColor3f(0.0f, 1.0f, 0.0f); 
+	glVertex3f(-1.0f, -1.0f, 1.0f); //left corner of front face
+
+	glColor3f(0.0f, 0.0f, 1.0f); 
+	glVertex3f(1.0f, -1.0f, 1.0f); //right corner of front face
+
+	 //Right Face
+	glColor3f(1.0f, 0.0f, 0.0f); 
+	glVertex3f(0.0f, 1.0f, 0.0f); //apex
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f); //left corner of Right face
+
+	glColor3f(0.0f, 1.0f, 0.0f); 
+	glVertex3f(1.0f, -1.0f, -1.0f); //right corner of Right face
+
+	//Back face
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f); //apex
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f); //left corner of Back face
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f); //right corner of Back face
+
+	//Left Face
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f); //apex
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f); //left corner of Back face
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f); //right corner of Back face
+
+	glEnd();
+
+	SwapBuffers(ghdc);
+}
+
+LRESULT CALLBACK AcCallBack(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	void uninitialize(void);
+	void resize(GLint width, GLint height);
+	void ToggleFullScreen(void);
+
+	switch (iMsg)
+	{
+	case WM_ACTIVATE:
+		if (HIWORD(wParam) == 0)
+		{
+			gbActiveWindow = true;
+		}
+		else
+		{
+			gbActiveWindow = false;
+		}
+		break;
+
+	case WM_ERASEBKGND:
+		return  0;
+
+	case WM_LBUTTONDOWN:
+		break;
+
+	case WM_SIZE:
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_CLOSE:
+		uninitialize();
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			if (gbEscapeKeyIsPressed == false)
+			{
+				gbEscapeKeyIsPressed = true;
+			}
+			break;
+
+		case 0x46:
+			if (gbFullScreen == false)
+			{
+				ToggleFullScreen();
+				gbFullScreen = true;
+			}
+			else
+			{
+				ToggleFullScreen();
+				gbFullScreen = false;
+			}
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
